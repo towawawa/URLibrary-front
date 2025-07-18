1dc735e14ce882448b4e7f20ee275b4e0c11e9dd
CTF@timamoto <t.imamoto@ctf-group.jp>
2025-07-18 15:05:41 +0900

    ネットリファイ設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBaseUrl: 'http://localhost:8080/api',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api',
     },
   },
 });
